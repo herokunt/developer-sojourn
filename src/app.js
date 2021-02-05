05ede5911dfb333cd389169cd5b549fadb17edb1
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-02-05 21:08:13 +0100

    added single article modal view

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import './styles.scss';
-// import './scripts/CanvasController';
-// import LazyLoader from './scripts/LazyLoader';
+import './scripts/CanvasController';
+import LazyLoader from './scripts/LazyLoader';
 import PostLoader from './scripts/PostLoader';
 
-// const isMobileDevice = navigator.userAgent.match(
-//   /(Android|iPhone|iPad|iPod|webOS|Windows Phone|BlackBerry)/i
-// );
+const isMobileDevice = navigator.userAgent.match(
+  /(Android|iPhone|iPad|iPod|webOS|Windows Phone|BlackBerry)/i
+);
 
 /* Only load videos if device is on desktop for better UX */
-// if (!isMobileDevice) {
-//   new LazyLoader();
-// }
+if (!isMobileDevice) {
+  new LazyLoader();
+}
 
 new PostLoader();
--- src/scripts/PostLoader.js
+++ src/scripts/PostLoader.js
@@ -5,6 +5,12 @@ class PostLoader {
     this.searchFilters = Array.from(this.form.querySelectorAll('.search__filter'));
     this.articles = document.querySelector('.articles');
 
+    this.postModal      = document.getElementById('post');
+    this.postModalClose = document.getElementById('post__close');
+    this.postModalTitle = document.getElementById('post__title');
+    this.postModalContent = document.getElementById('post__content');
+    this.postPreviousId = 0;
+
     this.currentFilters = [];
     this.prevQuery = '';
 
@@ -16,18 +22,28 @@ class PostLoader {
     this.events();
   }
 
+  /**
+  * Sets up event listeners to update filters
+  * @return {}
+  */
   events(){
     this.form.addEventListener('change', (e) => this.handleFilterUpdate(e));
     this.form.addEventListener('submit', (e) => e.preventDefault());
     this.searchField.addEventListener('keyup', () => this.handleKeyPress());
+    this.articles.addEventListener('click', (e) => this.handleRenderPost(e));
+    this.postModalClose.addEventListener('click', () => this.closeModal());
   }
 
+  /**
+  * Creates an observer to lazy load post section data
+  * @return {}
+  */
   createObserver() {
     const target = document.getElementById('section-notes');
 
     const options = {
       root: null,
-      rootMargin: '200px',
+      rootMargin: '0px',
       threshold: 0.0
     };
 
@@ -35,19 +51,12 @@ class PostLoader {
     this.observer.observe(target);
   };
 
-  // createListener() {
-  //   const target = document.getElementById('mutationTarget');
-  //
-  //   const options = {
-  //     characterData: true,
-  //     subtree: true,
-  //     childList: true
-  //   };
-  //
-  //   this.mutationObserver = new MutationObserver(console.log);
-  //   this.mutationObserver.observe(target, options);
-  // }
-
+  /**
+  * Checks if target node is intersecting with viewport and loads post data
+  * @param  {array} entries Observer's list of observed entries
+  * @param  {node}  target  Observer's target node
+  * @return {}
+  */
   handleLoadData(entries, target) {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -57,8 +66,12 @@ class PostLoader {
     });
   }
 
+  /**
+  * Hits an endpoint containing all the posts and assigns it to this.posts
+  * @return {}
+  */
   loadData(){
-    fetch('https://www.developersojourn.site/wp-json/wp/v2/posts')
+    fetch('https://developersojourn.site/wp-json/content/posts')
       .then(res => res.json())
       .then(posts => {
         const firstLoadPosts = posts.slice(0, 6);
@@ -68,23 +81,10 @@ class PostLoader {
       .catch(console.error);
   }
 
-  renderPosts(posts) {
-
-    if (!posts) {
-      this.articles.innerHTML = '<p>There are no results</p>';
-      return;
-    }
-
-    this.articles.innerHTML = '';
-    posts.forEach(post => {
-      const article = document.createElement('article');
-      article.className = 'articles__card';
-      article.insertAdjacentHTML('beforeend', post.title.rendered);
-      article.insertAdjacentHTML('beforeend', post.excerpt.rendered);
-      this.articles.appendChild(article);
-    });
-  }
-
+  /**
+  * Updates text filter and calls the render function. Uses a debounce function.
+  * @return {}
+  */
   handleKeyPress(){
     // Prevent unnecessary queries if input value hasn't changed
     if (this.previousSearchTerm !== this.searchField.value){
@@ -104,6 +104,10 @@ class PostLoader {
     }
   }
 
+  /**
+  * Updates tag filter and calls the render function.
+  * @return {}
+  */
   handleFilterUpdate(e) {
     if (e.target.type === 'checkbox') {
 
@@ -112,30 +116,80 @@ class PostLoader {
       }, []);
     }
 
-    console.log(this.currentFilters);
     const filtered = this.filterPosts();
     this.renderPosts(filtered);
   }
 
+  handleRenderPost(e) {
+    const id = e.target.postId;
+    if (!id) return;
+
+    if (id === this.postPreviousId) {
+      return this.openModal();
+    }
+
+    const { title, content } = this.posts.find(post => post.id === id);
+    console.log(this.postModal.lastChild)
+    this.postModalTitle.textContent = title;
+    this.postModalContent.innerHTML = '';
+    this.postModalContent.insertAdjacentHTML('beforeend', content);
+    this.previousId = id;
+
+    this.openModal();
+  }
+
+  openModal() {
+    this.postModal.classList.add('post--active');
+    this.postModalClose.classList.add('post__close--active');
+    document.body.classList.add('no-overflow');
+  }
+
+  closeModal() {
+    this.postModal.classList.remove('post--active');
+    this.postModalClose.classList.remove('post__close--active');
+    document.body.classList.remove('no-overflow');
+  }
+
+  /**
+  * Uses the text and tags filter to return a filtered array of posts.
+  * @return {array} An array of posts that match the filters criteria
+  */
   filterPosts() {
     const query   = this.searchField.value.trim().toLowerCase();
     const filters = this.currentFilters;
 
     return this.posts.filter(({ title, excerpt, tags }) => {
       const queryMatch = (
-        title.rendered.toLowerCase().includes(query) ||
-        excerpt.rendered.toLowerCase().includes(query)
+        title.toLowerCase().includes(query) ||
+        excerpt.toLowerCase().includes(query)
       );
       const filterMatch = filters.length ? filters.some(f => tags.includes(f)) : true;
 
       return queryMatch && filterMatch;
     });
   }
+
+  /**
+  * Renders a list of posts provided as a paremeter
+  * @param  {array} posts An array of posts to be rendered to the DOM
+  * @return {}
+  */
+  renderPosts(posts) {
+    if (!posts) {
+      this.articles.innerHTML = '<p>There are no results</p>';
+      return;
+    }
+
+    this.articles.innerHTML = '';
+    posts.forEach(post => {
+      const article = document.createElement('article');
+      article.className = 'articles__card';
+      article.postId = post.id;
+      article.insertAdjacentHTML('beforeend', post.title);
+      article.insertAdjacentHTML('beforeend', post.excerpt);
+      this.articles.appendChild(article);
+    });
+  }
 }
 
 export default PostLoader;
-
-
-// const matches = this.posts.filter(post => {
-//   return post.title.toLowerCase().includes(query) || post.excerpt.toLowerCase().includes(query)
-// })
